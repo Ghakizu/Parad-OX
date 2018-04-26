451e64259a7b3402fb50a2c3ea9b3a03e76a80ff
Unknown <cmalanda1999@outlook.fr>
2018-04-27 00:42:04 +0200

    Lobby toujours casse

---
+++ Assets/Scripts/Multiplayer/Network/LobbyNetwork.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyNetwork : MonoBehaviour
+{
+    private string Version = "Alpha v0.0.1";
+/*
+    // Use this for initialization
+    private void Start()
+    {
+        if (!PhotonNetwork.connected)
+        {
+            print("Connecting to server..");
+            PhotonNetwork.ConnectUsingSettings("0.0.0");
+        }
+    }
+
+    private void OnConnectedToMaster()
+    {
+        print("Connected to master.");
+        PhotonNetwork.automaticallySyncScene = true;
+       // PhotonNetwork.playerName = PlayerNetwork.Instance.PlayerName;
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+
+    private void OnJoinedLobby()
+    {
+        print("Joined lobby.");
+
+        if (!PhotonNetwork.inRoom) ;
+         //   MainCanvasManager.Instance.LobbyCanvas.transform.SetAsLastSibling();
+    } */
+
+    void Start ()
+    {
+
+    }
+
+    public void OnClick_LobbyNetwork()
+    {
+        Debug.LogError("Connecting to server...");
+        PhotonNetwork.ConnectUsingSettings(Version);
+    }
+
+    private void Update()
+    {
+        Debug.LogError(PhotonNetwork.connectionState);
+    }
+
+    private void OnConnectedToMaster()
+    {
+        Debug.LogError("Connected to master.");
+        PhotonNetwork.playerName = PlayerNetwork.Instance.PlayerName;
+
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+
+    private void OnJoinedLobby()
+    {
+        Debug.LogError("Joined lobby");
+
+    }
+}
---
+++ Assets/Scripts/Multiplayer/RoomLayoutGroup.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomLayoutGroup : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _roomListingPrefab;
+    private GameObject RoomListingPrefab
+    {
+        get { return _roomListingPrefab; }
+    }
+
+    private List<RoomListing> _roomListingButtons = new List<RoomListing>();
+    private List<RoomListing> RoomListingButtons
+    {
+        get { return _roomListingButtons; }
+    }
+
+    private void OnReceivedRoomListUpdate()
+    {
+        RoomInfo[] rooms = PhotonNetwork.GetRoomList();
+        int length = rooms.Length;
+
+        for(int i = 0; i < length; i++)
+        {
+            RoomReceived(rooms[i]);
+        }
+
+        RemoveOldRooms();
+    }
+
+    private void RoomReceived(RoomInfo room)
+    {
+        int i = RoomListingButtons.FindIndex(x => x.RoomName == room.Name);
+
+        if (i == -1)
+        {
+            if(room.IsVisible && room.PlayerCount < room.MaxPlayers)
+            {
+                GameObject roomListingObj = Instantiate(RoomListingPrefab);
+                roomListingObj.transform.SetParent(transform, false);
+
+                RoomListing roomListing = roomListingObj.GetComponent<RoomListing>();
+                RoomListingButtons.Add(roomListing);
+
+                i = RoomListingButtons.Count - 1;
+            }
+        }
+        else
+        {
+            RoomListing roomListing = RoomListingButtons[i];
+            roomListing.SetRoomNameText(room.Name);
+            roomListing.Updated = true;
+        }
+    }
+
+    private void RemoveOldRooms()
+    {
+        List<RoomListing> removeRooms = new List<RoomListing>();
+        int length = RoomListingButtons.Count;
+
+        for(int i = 0; i < length; i++)
+        {
+            RoomListing roomListing = RoomListingButtons[i];
+            if (!roomListing.Updated)
+                removeRooms.Add(roomListing);
+            else
+                roomListing.Updated = false;
+        }
+
+        length = removeRooms.Count;
+
+        for(int i = 0; i < length; i++)
+        {
+            RoomListing roomListing = removeRooms[i];
+            GameObject roomListingObj = roomListing.gameObject;
+            RoomListingButtons.Remove(roomListing);
+            Destroy(roomListingObj);
+        }
+    }
+}
