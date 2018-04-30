4772878d9877397fb58f88b6fcaf409282564aaa
Pokkihju <cyril.rideau@epita.fr>
2018-04-30 22:23:26 +0100

    Animations du futur
    Attack, Idle, Run

--- Assets/Scripts/Multiplayer/Network/MultiTemp.cs
+++ Assets/Scripts/Multiplayer/Network/MultiTemp.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class MultiTemp : MonoBehaviour
 {
+	public int x = 0;
+	public int y = 0;
+	public int z = 0;
 
     // Use this for initialization
     void Start()
     {
         PhotonNetwork.offlineMode = true;
         PhotonNetwork.CreateRoom("Solo");
+		Vector3 position = new Vector3 (x, y, z);
+		PhotonNetwork.Instantiate ("_Player", position, Quaternion.identity, 0);
      }
 }
