b894765260f562129899ad7f96e91c4a5b9d3270
Axel <axel.grunenberger@epita.fr>
2018-05-02 00:59:40 +0200

    saving my work <3

---
+++ Assets/Scripts/Objects/AttackingObjects/Consumables/SpeedPotion.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedPotion : _Consumables {
+
+	new public void Awake()
+	//set all the stats of the item
+	{
+		ObjectName = "SpeedPotion";
+		sprite = MaterialsAssignations.HealthPotionSprite;
+		damages = 20; 
+		RangeOfAttk = 0;
+		TimeBetweenAttacks = 10;
+		description = "Increase the speed of your player for " + damages + " seconds.";
+		base.Awake ();
+	}
+
+
+
+	public void UseItem()
+	//Launch the spell AirWall on us
+	{
+		//owner.Health += damages;
+		//owner.Health = Mathf.Min (owner.Health, owner.MaxHealth);
+	}
+
+}
