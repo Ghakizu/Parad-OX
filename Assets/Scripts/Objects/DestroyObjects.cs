5780aba8014a9bcb3dfbd1f8929d2705d95fc6b8
Pokkihju <cyril.rideau@epita.fr>
2018-05-02 10:38:09 +0100

    All Buttons and Destroy/Deactivate scripts

--- Assets/Scripts/Objects/DestroyObjects.cs
+++ Assets/Scripts/Objects/DestroyObjects.cs
@@ -7,15 +7,53 @@ public class DestroyObjects : MonoBehaviour {
 
 	public GameObject Restriction;
 	private PhotonView photonView;
+	private float timer;
+	private bool timerstarted = false;
+	public bool destroy;
 
 	void Awake()
 	{
 		photonView = GetComponent<PhotonView> ();
 	}
 
+	void Update ()
+	{
+		if (timerstarted) 
+		{
+			if (timer > 0)
+				timer -= Time.deltaTime;
+			else 
+			{
+				photonView.RPC ("Activate_Wall", PhotonTargets.All, Restriction);
+				timerstarted = false;
+			}
+		}
+	}
+
 	void OnMouseDown ()
 	{
-		photonView.RPC ("Destroy_Wall", PhotonTargets.All, Restriction);
+		if (destroy) 
+		{
+			photonView.RPC ("Destroy_Wall", PhotonTargets.All, Restriction);
+		} 
+		else 
+		{
+			photonView.RPC ("Deactivate_Wall", PhotonTargets.All, Restriction);
+			timer = 5;
+			timerstarted = true;
+		}
+	}
+
+	[PunRPC]
+	void Deactivate_Wall(GameObject Restriction)
+	{
+		Restriction.SetActive (false);
+	}
+
+	[PunRPC]
+	void Activate_Wall(GameObject Restriction)
+	{
+		Restriction.SetActive (true);
 	}
 
 	[PunRPC]
