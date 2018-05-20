c71767d714e8de90f7090865511cc961518fdcf0
Pokkihju <cyril.rideau@epita.fr>
2018-05-20 15:32:11 +0100

    Beginning of Enigma Level and scripts for buttons.

---
+++ Assets/Scripts/Objects/Destroy_Solo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_Solo : MonoBehaviour {
+
+	public GameObject Door;
+
+	void OnMouseDown()
+	{
+		GameObject.Destroy (Door);
+	}
+}
---
+++ Assets/Scripts/Objects/Trapped.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trapped : MonoBehaviour {
+
+	public float x;
+	public float y;
+	public float z;
+	public GameObject player;
+
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void Update ()
+	{
+		if (player == null) {
+			player = GameObject.FindGameObjectWithTag ("Player");
+		}
+	}
+
+	void OnMouseDown ()
+	{
+		player.transform.position = new Vector3 (x, y, z);
+	}
+}
