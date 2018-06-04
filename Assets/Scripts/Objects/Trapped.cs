66308cb383a5ac37cf3205b970d2e3011e6f0d8b
Pokkihju <cyril.rideau@epita.fr>
2018-06-04 18:45:45 +0200

    Scene + spawn

--- Assets/Scripts/Objects/Trapped.cs
+++ Assets/Scripts/Objects/Trapped.cs
@@ -9,11 +9,30 @@ public class Trapped : MonoBehaviour {
 	public float z;
     public bool mainRoom;
 	public GameObject player;
+    public int coordinatex;
+    public int coordinatey;
+    public int coordinatez;
+    private GameObject ennemy;
+
+    void Awake()
+    {
+        ennemy = (GameObject)Resources.Load("Fists enemy");
+    }
 
 	void OnMouseDown ()
 	{
         player = GameObject.FindGameObjectWithTag("Player");
         if (mainRoom)
-		    player.transform.position = new Vector3 (x, y, z);
+            player.transform.position = new Vector3(x, y, z);
+        else
+            SpawnEnnemies();
 	}
+
+    private void SpawnEnnemies()
+    {
+        GameObject.Instantiate(ennemy, new Vector3(coordinatex + 150, coordinatey, coordinatez), new Quaternion());
+        GameObject.Instantiate(ennemy, new Vector3(coordinatex + 50, coordinatey, coordinatez), new Quaternion());
+        GameObject.Instantiate(ennemy, new Vector3(coordinatex - 50, coordinatey, coordinatez), new Quaternion());
+        GameObject.Instantiate(ennemy, new Vector3(coordinatex - 150, coordinatey, coordinatez), new Quaternion());
+    }
 }
